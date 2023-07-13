eba913ab6e8abefde5d626eba69104c6f4014577
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-07-13 13:02:10 +0700

    finish search page and then fix sth on popular jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -22,7 +22,10 @@ const Popularjobs = () => {
 
   const [selectedJob, setSelectedJob] = useState();
 
-  const handleCardPress = () => {};
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
